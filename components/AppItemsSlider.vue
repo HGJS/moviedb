65e3eeb9e3f06cb3a231ca8c6bbbf8d91debe429
Sam Hellewell <samhellewell@gmail.com>
2020-08-21 20:23:21 +0100

    Updates to sliders

--- components/AppItemsSlider.vue
+++ components/AppItemsSlider.vue
@@ -49,18 +49,20 @@ export default {
 				return {
 					arrows: false,
 					slidesToShow: 4,
-					slidesToScroll: 1,
+					slidesToScroll: 4,
 					responsive: [
 						{
 							breakpoint: 1024,
 							settings: {
-								slidesToShow: 3
+								slidesToShow: 3,
+								slidesToScroll: 3
 							}
 						},
 						{
 							breakpoint: 768,
 							settings: {
-								slidesToShow: 2
+								slidesToShow: 2,
+								slidesToScroll: 2
 							}
 						}
 					]
@@ -69,12 +71,13 @@ export default {
 				return {
 					arrows: false,
 					slidesToShow: 2,
-					slidesToScroll: 1,
+					slidesToScroll: 2,
 					responsive: [
 						{
 							breakpoint: 768,
 							settings: {
-								slidesToShow: 1
+								slidesToShow: 1,
+								slidesToScroll: 1
 							}
 						}
 					]
