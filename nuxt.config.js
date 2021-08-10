780f152807adf1144eb291aa65aff77be2288452
Sam Hellewell <samhellewell@gmail.com>
2021-08-10 20:12:02 +0100

    Update font awesome import

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,18 +19,21 @@ export default {
 				content: process.env.npm_package_description || ''
 			}
 		],
-		script: [
-			{
-				src: 'https://kit.fontawesome.com/11d4c6a015.js',
-				crossorigin: 'anonymous'
-			}
-		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
 			{
 				rel: 'stylesheet',
 				href:
 					'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap'
+			},
+			{
+				rel: 'stylesheet',
+				href:
+					'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
+				integrity:
+					'sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==',
+				crossorigin: 'anonymous',
+				referrerpolicy: 'no-referrer'
 			}
 		]
 	},
